0a6ab67ac52395aeb91ed9f8cdeca6b49de09bbf
Matt Eichsteadt <matt@>
2016-10-17 16:42:42 -0700

    added temperature, bmi and cups conversion calculation

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,35 @@ var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter another number: "));
 
 alert(divide(number1, number2));
+
+var bmiCalculation = function(height, weight){
+  return ( weight / ( height * height ) ) * 703;
+}
+var height = parseInt(prompt("Please enter your height in inches: "));
+var weight = parseInt(prompt("Please enter your weight in pounds: "));
+
+alert("Based on your height of: " +height+ " and weight of: " +weight+", your BMI is: "+ bmiCalculation(height, weight) + ".");
+
+var unit = ""
+var tempConversion = function(temp,type){
+  if (type === "C"){
+    return (temp * 1.8) +32;
+    unit = "F";
+  }
+  else{
+    return(temp-32)/1.8;
+    unit = "C";
+  }
+}
+var temp = parseInt(prompt("Please enter your temperature: "));
+var type = prompt('Please specify the unit using "C" for celsius and "F" for fahrenheit: ');
+
+alert("Your temperature in " + type + " is equal to " + tempConversion(temp, type) + unit + ".");
+
+//This function converts tablespoons to cups
+function tbspToCup (tbsp) {
+	tbsp = parseInt(prompt("How many tablespoons do you have?"));
+  return "That is " + tbsp * .0625 + " cups";
+};
+
+alert(tbspToCup());
